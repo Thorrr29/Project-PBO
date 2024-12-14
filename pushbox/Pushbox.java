12ecbdf45021dcb81974cccc91c25012717759b8
Fathurrahman29 <151018153+Thorrr29@users.noreply.github.com>
2024-12-14 20:44:11 +0700

    Update Pushbox.java

--- pushbox/Pushbox.java
+++ pushbox/Pushbox.java
@@ -1,10 +1,10 @@
-
 package pushbox;
 
+import java.net.URL;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import static javafx.application.Application.launch;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 /**
@@ -13,13 +13,26 @@ import javafx.stage.Stage;
  */
 public class Pushbox extends Application {
     
+    private MediaPlayer mediaPlayer;
     @Override
     public void start(Stage stage) {
         // Show the start screen
         StartScreen startScreen = new StartScreen(stage);
         startScreen.show();
+        playBackgroundMusic();
     }   
-
+    
+    private void playBackgroundMusic() {
+        URL musicUrl = getClass().getResource("resource/backsound.mp3");
+        if (musicUrl != null) {
+            String musicPath = musicUrl.toExternalForm();
+            Media media = new Media(musicPath);
+            mediaPlayer = new MediaPlayer(media);
+            mediaPlayer.setVolume(0.5);
+            mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+            mediaPlayer.setAutoPlay(true);
+        }
+    }
     /**
      * @param args the command line arguments
      */
