b33b39d9ab590aa2be9aa5f6e7bed70695a638c1
Fathurrahman29 <151018153+Thorrr29@users.noreply.github.com>
2024-12-14 14:36:07 +0700

    Update player.java

--- pushbox/player.java
+++ pushbox/player.java
@@ -1,1 +1,14 @@
+package pushbox;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Player extends ImageView {
+    public Player(double tileSize, double x, double y) {
+        super(new Image(Player.class.getResourceAsStream("player.png")));
+        setFitWidth(tileSize);
+        setFitHeight(tileSize);
+        setX(x);
+        setY(y);
+    }
+}
